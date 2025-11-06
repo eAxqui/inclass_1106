015aac74053b12349c54e1ad4ed3bc8b75954d50
jericacilo <eric.acilo@live.com>
2025-11-06 15:35:02 -0700

    init get age function

--- inclass_1106.py
+++ inclass_1106.py
@@ -1,2 +1,7 @@
-def get_age():
-    return 0
+def get_age(name, year_of_birth):
+    print("Welcome ", name)
+    age = 2025 - year_of_birth
+    print("Your age is ", age)
+
+
+get_age("John", 1997)
