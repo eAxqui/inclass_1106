b77f7b86252f00836c3c71b9dfeb17b834dc75b5
jericacilo <eric.acilo@live.com>
2025-11-06 17:27:32 -0700

    cleaned up to just show functions

--- myutils.py
+++ myutils.py
@@ -3,9 +3,6 @@ def get_age(name, year_of_birth):
     age = 2025 - year_of_birth
     print("Your age is ", age)
 
-
-get_age("John", 1997)
-
 # Celsius to Fahrenheit conveter function
 def cels_to_fah(celsius):
     fahrenheit = (9/5) * celsius +32
@@ -17,18 +14,3 @@ def fah_to_cels(fahrenheit):
     celsius = (fahrenheit - 32) * 5 / 9
 
     return celsius
-
-# Output test for celsius to fahrenheit
-answer_one = cels_to_fah(30)
-print(answer_one)
-
-# Output test for fahrenheit to celsius
-answer_two = fah_to_cels(86)
-print(answer_two)
-
-
-"""
-Documentation comment init
-"""
-
-print(print.__doc__)
