a8ece4db2655d6f54780c8c7d26bd400a1837f8e
jericacilo <eric.acilo@live.com>
2025-11-06 17:11:15 -0700

    comments update

--- inclass_1106.py
+++ inclass_1106.py
@@ -6,18 +6,22 @@ def get_age(name, year_of_birth):
 
 get_age("John", 1997)
 
+# Celsius to Fahrenheit conveter function
 def cels_to_fah(celsius):
     fahrenheit = (9/5) * celsius +32
 
     return fahrenheit
 
+# Fahrenheit to Celsius converter function
 def fah_to_cels(fahrenheit):
     celsius = (fahrenheit - 32) * 5 / 9
 
     return celsius
 
+# Output test for celsius to fahrenheit
 answer_one = cels_to_fah(30)
 print(answer_one)
 
+# Output test for fahrenheit to celsius
 answer_two = fah_to_cels(86)
 print(answer_two)
