32680e4ebc0d3ec4ffd7cb9ea0befe77b2f28c29
jericacilo <eric.acilo@live.com>
2025-11-06 17:26:46 -0700

    imported code from inclass to myutils

---
+++ myutils.py
@@ -0,0 +1,34 @@
+def get_age(name, year_of_birth):
+    print("Welcome ", name)
+    age = 2025 - year_of_birth
+    print("Your age is ", age)
+
+
+get_age("John", 1997)
+
+# Celsius to Fahrenheit conveter function
+def cels_to_fah(celsius):
+    fahrenheit = (9/5) * celsius +32
+
+    return fahrenheit
+
+# Fahrenheit to Celsius converter function
+def fah_to_cels(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9
+
+    return celsius
+
+# Output test for celsius to fahrenheit
+answer_one = cels_to_fah(30)
+print(answer_one)
+
+# Output test for fahrenheit to celsius
+answer_two = fah_to_cels(86)
+print(answer_two)
+
+
+"""
+Documentation comment init
+"""
+
+print(print.__doc__)
